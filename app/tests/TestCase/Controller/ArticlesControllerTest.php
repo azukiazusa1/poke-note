d4e4387cd40b8738300349e3d34e447ee97d922c
azukiazusa <>
2020-03-02 22:19:56 +0900

    はじめのコントローラテスト

---
+++ app/tests/TestCase/Controller/ArticlesControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+class ArticlesControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    public $fixtures = ['app.Articles', 'app.Users', 'app.Tags', 'app.ArticlesTags'];
+
+    public function testIndex()
+    {
+        $this->get('/');
+
+        $this->assertResponseOk();
+        // 他のアサート
+    }
+}
