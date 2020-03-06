3ff77a15fc14f44bf3c2474b1052faa09edb9b32
azukiazusa <>
2020-03-06 21:45:28 +0900

    プロフィール編集画面

--- app/tests/TestCase/Controller/UsersControllerTest.php
+++ app/tests/TestCase/Controller/UsersControllerTest.php
@@ -45,4 +45,25 @@ class UsersControllerTest extends TestCase
         $this->get('/users/notExsitsUser');
         $this->assertResponseCode(404);
     }
+
+    public function プロフィール編集画面()
+    {
+        $this->session(['Auth.User.id' => 1]);
+        $this->get('/profile');
+
+        $this->assertResponseContains('@user1');
+
+        $user = $this->viewVariable('user');
+
+        $this->assertInstanceOf(User::class,$user);
+        $this->assertSame('user1', $user->username);
+    }
+
+    public function プロフィール編集画面はログインが必要()
+    {
+        $this->get('/profile');
+
+        $this->assertResponseCode(302);
+        $this->assertRedirect('/login?redirect=%profile');
+    }
 }
